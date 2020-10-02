938b48b32a0e3baa84c19f14483089f28024a488
Elias Peteri <elias.peteri@hotmail.com>
2020-10-02 11:05:31 +0300

    project plan

---
+++ scraper.py
@@ -0,0 +1,9 @@
+import requests
+import json
+import re 
+
+# tuo data THL:n rajapinnasta JSON-oliona
+# pura JSON ja erittele haluttu data listan tai olion alkioihin
+# tulosta lista visuaalisesti esim matplotlibilla
+# valinnaisesti kysy käyttäjältä haluttu shp tai paikkakunta
+# tulosta CSV-tiedostoon?
