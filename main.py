69f1a092db508acb9740972a4c11caaf7f258e01
Elias Peteri <eliaspeteri@localhost.localdomain>
2020-10-24 18:56:00 +0300

    Fix: Refactored code

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+# dependencies
 import requests
-from datetime import datetime, date, time
-# Used source is from https://github.com/HS-Datadesk/koronavirus-avoindata
-url = 'https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaHospitalData'
-
-# import data from the API as JSON
-response = requests.get(url).json()
+import matplotlib.pyplot as plt
+from matplotlib import dates, pyplot
+import numpy as np
+from datetime import datetime
 
 # date format is assumed to be mmdd and year is 2020
 
@@ -13,21 +12,54 @@ response = requests.get(url).json()
 # hospital = desired hospital's data
 # timeframe = desired timeframe
 # prints the data fetched from the API with the set parameters limiting the scope of data shown
+
 def printHospitalCases(hospital, timeframe):
+    # initialise the variables used later in the code
     totalHospitalised = 0
     inWard = 0
     inIcu = 0
     dead = 0
+    # check if a specific timeframe is given and create string variables to limit the search with
+    if(timeframe != ""):
+        startQuery = "2020-{}{}-{}{}{}".format(timeframe[0], timeframe[1], timeframe[2], timeframe[3], "T13:00:00.000Z")
+        endQuery = "2020-{}{}-{}{}{}".format(timeframe[5], timeframe[6], timeframe[7], timeframe[8], "T13:00:00.000Z")
+
+    # search through the response
     for hospital_q in response["hospitalised"]:
+        queryDate = hospital_q["date"]
         if(hospital_q["area"] == hospital):
-            totalHospitalised = hospital_q["totalHospitalised"]
-            inWard = hospital_q["inWard"]
-            inIcu = hospital_q["inIcu"]
-            dead = hospital_q["dead"]
+            # if a specific timeframe is given, it is checked for here
+            if(timeframe != "" and queryDate >= startQuery and queryDate <= endQuery):
+                totalHospitalised = hospital_q["totalHospitalised"]
+                inWard = hospital_q["inWard"]
+                inIcu = hospital_q["inIcu"]
+                dead = hospital_q["dead"]
+            # if a specific timeframe is not given, print the latest result
+            elif(timeframe == ""):
+                totalHospitalised = hospital_q["totalHospitalised"]
+                inWard = hospital_q["inWard"]
+                inIcu = hospital_q["inIcu"]
+                dead = hospital_q["dead"]
+                
+    #print the result
     print("\nHospital: {}\nTotal hospitalised: {}\nWard: {}\nICU: {}\nDead: {}\n".format(hospital, totalHospitalised, inWard, inIcu, dead))
 
-# ask the user for the healthcare district and optional specific time period
-printHospitalCases(input("Please select hospital by code (HYKS, KYS, OYS, TAYS, TYKS, Finland): ")
-                  ,input("\nSelect start and end date in format (mmdd)-(mmdd). Year is assumed to be 2020\nEmpty query prints everything: "))
+def dialog():
+    cont = True
+    while(cont):
+        # ask the user for the healthcare district and optional specific time period
+        printHospitalCases(input("Please select hospital by code (HYKS, KYS, OYS, TAYS, TYKS, Finland): ")
+                        ,input("\nSelect start and end date in format (mmdd)-(mmdd) without parentheses. Year is assumed to be 2020\nEmpty query prints everything: "))
+        if(input("Continue? Y/N: ") == 'N'):
+            cont = False
+
+# Used source is from https://github.com/HS-Datadesk/koronavirus-avoindata
+url = 'https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaHospitalData'
+
+# import data from the API as JSON
+response = requests.get(url).json()
+
+# runtime
+dialog()
 
 # optionally export to a CSV
